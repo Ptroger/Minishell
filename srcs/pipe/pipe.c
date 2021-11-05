1d07935e10f29438b10eed3957ba02705c508138
Aristide BESNIER <aribesni@e3r5p5.42.fr>
2021-11-05 17:33:53 +0100

    multiple pipes almost done

--- srcs/pipe/pipe.c
+++ srcs/pipe/pipe.c
@@ -41,28 +41,19 @@ void	ft_add_elem_pipe(t_pipe **store)
 	*store = new_elem;
 }
 
-void  ft_process()
+void  ft_process(t_pipe *store, char **tab, int **pfd, pid_t *child_1)
 {
    int      i;
-   int      status;
-   int      pfd[2];
    char     *token_1;
-   char     *token_2;
    char     **cmd_1;
-   char     **cmd_2;
-   pid_t    child_1;
-   pid_t    child_2;
+//   pid_t    child_1;
    t_pipe   *temp_p;
 
-   if (pipe(pfd) == -1)
-   {
-      printf("pipe failed\n");
-      return ;
-   }
    i = 0;
    temp_p = store;
-   while (temp_p)
-   {
+//   while (temp_p)
+//   {
+   printf("HERE\n");
       token_1 = ft_strdup(temp_p->cell[i]);
       cmd_1 = ft_command_size(temp_p->size + 1);
       if (temp_p->size > 1)
@@ -73,45 +64,66 @@ void  ft_process()
             i++;
          }
       }
-      child_1 = fork();
-      if (child_1 < 0)
+      *child_1 = fork();
+      if (*child_1 < 0)
       {
          printf("fork failed\n");
          return ;
       }
-      if (child_1 == 0)
+      if (*child_1 == 0)
       {
-         close(pfd[0]);
-         dup2(pfd[1], 1);
-         close(pfd[1]);
+         close(*pfd[1]);
+         dup2(*pfd[0], 0);
+         close(*pfd[0]);
          ft_find_cmd(token_1, cmd_1, tab);
       }
-   }
+//   }
 }
 
-void  ft_child(t_pipe *store, char **tab)
+void  ft_child(t_pipe *store, char **tab, int size)
 {
+   int      a;
+   int      b;
+   int      count;
    int      i;
    int      status;
-   int      pfd[2];
+   int      *pfd;
    char     *token_1;
-   char     *token_2;
+//   char     *token_2;
+//   char     *token_3;
    char     **cmd_1;
-   char     **cmd_2;
+//   char     **cmd_2;
+//   char     **cmd_3;
    pid_t    child_1;
-   pid_t    child_2;
+//   pid_t    child_2;
+//   pid_t    child_3;
    t_pipe   *temp_p;
 
-   if (pipe(pfd) == -1)
-   {
-      printf("pipe failed\n");
-      return ;
-   }
    i = 0;
+   if (size % 2 != 0)
+      size++;
+   pfd = (int*)malloc(sizeof(int) * size);
+   if (!pfd)
+      return ;
+   while (i < size)
+   {
+      if (pipe(pfd + i) == -1)
+      {
+         printf("pipe failed\n");
+         return ;
+      }
+      printf("HERE\n");
+      i += 2;
+   }
    temp_p = store;
+   a = 1;
+   b = 1;
+   count = 0;
    while (temp_p)
    {
+      i = 0;
       token_1 = ft_strdup(temp_p->cell[i]);
+      printf("token : %s\n", token_1);
       cmd_1 = ft_command_size(temp_p->size + 1);
       if (temp_p->size > 1)
       {
@@ -127,15 +139,35 @@ void  ft_child(t_pipe *store, char **tab)
          printf("fork failed\n");
          return ;
       }
+      i = 0;
       if (child_1 == 0)
       {
-         close(pfd[0]);
-         dup2(pfd[1], 1);
-         close(pfd[1]);
+         if (count > 0 && count < size - 1)
+            dup2(pfd[a - 1], 0);
+         dup2(pfd[a], 1);
+         while (i < size)
+         {
+            close(pfd[i]);
+            i++;
+         }
          ft_find_cmd(token_1, cmd_1, tab);
       }
+   //   if (count == 0)
+   //   {
+   //      a++;
+   //      b--;
+         count++;
+   //   }
+      a += 2;
+      temp_p = temp_p->next;
    }
-/*   token_1 = ft_strdup(temp_p->cell[i]);
+//   ft_process(store, tab, &pfd, &child_2);
+/*   i = 0;
+   temp_p = store;
+   pfd = (int*)malloc(sizeof(int) * size + 1);
+   pipe(pfd);
+   pipe(pfd + 2);
+   token_1 = ft_strdup(temp_p->cell[i]);
    cmd_1 = ft_command_size(temp_p->size + 1);
    if (temp_p->size > 1)
    {
@@ -151,15 +183,18 @@ void  ft_child(t_pipe *store, char **tab)
       printf("fork failed\n");
       return ;
    }
+   i = 0;
    if (child_1 == 0)
    {
-      close(pfd[0]);
       dup2(pfd[1], 1);
-      close(pfd[1]);
+      while (pfd[i])
+      {
+         close(pfd[i]);
+         i++;
+      }
       ft_find_cmd(token_1, cmd_1, tab);
    }
    i = 0;
-   temp_p = store;
    temp_p = temp_p->next;
    token_2 = ft_strdup(temp_p->cell[i]);
    cmd_2 = ft_command_size(temp_p->size + 1);
@@ -177,42 +212,92 @@ void  ft_child(t_pipe *store, char **tab)
       printf("fork failed\n");
       return ;
    }
+   i = 0;
    if (child_2 == 0)
    {
-      close(pfd[1]);
       dup2(pfd[0], 0);
-      close(pfd[0]);
+      dup2(pfd[3], 1);
+      while (pfd[i])
+      {
+         close(pfd[i]);
+         i++;
+      }
       ft_find_cmd(token_2, cmd_2, tab);
    }
-*/   close(pfd[1]);
-   close(pfd[0]);
-   waitpid(child_1, &status, 0);
-   waitpid(child_2, &status, 0);
+   i = 0;
+   temp_p = temp_p->next;
+   token_3 = ft_strdup(temp_p->cell[i]);
+   cmd_3 = ft_command_size(temp_p->size + 1);
+   if (temp_p->size > 1)
+   {
+      while (i < temp_p->size - 1)
+      {
+         cmd_3[i + 1] = ft_strdup(temp_p->cell[i + 1]);
+   //      printf("%s\n", cmd_3[i + 1]);
+         i++;
+      }
+   }
+   child_3 = fork();
+   if (child_3 < 0)
+   {
+      printf("fork failed\n");
+      return ;
+   }
+   i = 0;
+   if (child_3 == 0)
+   {
+      dup2(pfd[2], 0);
+      while (pfd[i])
+      {
+         close(pfd[i]);
+         i++;
+      }
+      ft_find_cmd(token_3, cmd_3, tab);
+   }
+*/   i = 0;
+   printf("size : %i\n", size);
+   while (i < size)
+   {
+      close(pfd[i]);
+      printf("i : %i\n", i);
+      i++;
+   }
+   i = 0;
+   while (i < size - 1)
+   {
+      wait(&status);
+      i++;
+   } 
+//   waitpid(child_1, &status, 0);
+//   waitpid(child_2, &status, 0);   
+//   waitpid(child_3, &status, 0);   
 }
 
 int  ft_pipe(t_list *tokens, t_pipe *store, char **tab)
 {
    int      i;
    int      j;
+   int      size;
 
    t_list   *temp_1;
    t_list   *temp_2;
    t_pipe   *temp_p;
 
    i = 0;
+   size = 0;
    temp_1 = tokens;
    temp_2 = tokens;
    while (temp_1)
    {
       if (ft_strcmp(temp_1->token, "|") == 0)
-         i++;
+         size++;
       temp_1 = temp_1->next;
    }
-   i++;
-   while (i > 0)
+   size++;
+   while (i < size)
    {
       ft_add_elem_pipe(&store);
-      i--;
+      i++;
    }
    temp_1 = tokens;
    temp_p = store;
@@ -242,6 +327,6 @@ int  ft_pipe(t_list *tokens, t_pipe *store, char **tab)
       temp_p->size = j;
       temp_p = temp_p->next;
    }
-   ft_child(store, tab);
+   ft_child(store, tab, size);
    return (0);
 }
