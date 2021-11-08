c63000db519dd1e4c081739cf15e287b4cbe9736
Pierre Troger <pierre.troger@gmail.com>
2021-11-08 15:39:05 +0100

    lib + handle dollars encore un bug sur un cas

--- srcs/token/parse.c
+++ srcs/token/parse.c
@@ -1,22 +1,24 @@
-#include "../includes/minishell.h"
+#include "../../includes/minishell.h"
 
 char	*add_char_to_token(char c, t_vars *vars, int i, char *token)
 {
 	char	*new_token;
 	char	*ptr;
 
-//	printf("ici j == %d --- c == %c\n", i, c);
 	if (i < vars->token_size)
 		token[i] = c;
 	else
 	{
 		ptr = token;
 		vars->token_size *= 2;
-		new_token = malloc(sizeof(char) * vars->token_size);
+		new_token = malloc(sizeof(char) * vars->token_size + 1);
+		new_token[vars->token_size] = '\0';
 		ft_strcpy(token, new_token, i);
 		token = new_token;
+		token[i] = c;
 		free(ptr);
 	}
+	vars->token_i++;
 	return (token);
 }
 
@@ -63,10 +65,7 @@ int	handle_quotes(t_vars *vars, char *token, char *line)
 int	handle_space(t_vars *vars, char *token, char *line)
 {
 	if (vars->state == D_QUOTE || vars->state == S_QUOTE)
-	{
 		token = add_char_to_token(' ', vars, vars->token_i, token);
-		vars->token_i++;
-	}
 	else if (vars->parse_i >= 1 && line[vars->parse_i - 1] != ' ' && vars->state == BASIC)
 	{
 		token = add_char_to_token('\0', vars, vars->token_i, token);
@@ -78,10 +77,21 @@ int	handle_space(t_vars *vars, char *token, char *line)
 
 int	handlers(t_vars *vars, char *token, char *line)
 {
-	if (line[vars->parse_i] == '\'' || line[vars->parse_i] == '"')
+	if (line[vars->parse_i] == '$')
+	{
+		printf("vars->parse_i == %d\n", vars->parse_i);
+		if (handle_dollar(vars, token, line) == 1)
+		{
+			return (1);
+		}
+		printf("DANS token %s|\n", token);
+		printf("vars->parse_i == %d\n", vars->parse_i);
+	}
+	else if (line[vars->parse_i] == '\'' || line[vars->parse_i] == '"')
 	{
 		if (handle_quotes(vars, token, line) == 1)
 			return (1);
+		printf("state = %d\n", vars->state);
 	}
 	else if (line[vars->parse_i] == ' ')
 	{
@@ -89,10 +99,7 @@ int	handlers(t_vars *vars, char *token, char *line)
 			return (1);
 	}
 	else if (line[vars->parse_i] != ' ' || vars->state == D_QUOTE || vars->state == S_QUOTE)
-	{
 		token = add_char_to_token(line[vars->parse_i], vars, vars->token_i, token);
-		vars->token_i++;
-	}
 	return (0);
 }
 
@@ -102,7 +109,6 @@ char	*get_next_token(char *line, t_vars *vars)
 
 	token = malloc(sizeof(char) * vars->token_size + 1);
 	token[vars->token_size] = '\0';
-	token[0] = '\0';
 	while (line[vars->parse_i])
 	{
 		if (handlers(vars, token, line) == 1)
